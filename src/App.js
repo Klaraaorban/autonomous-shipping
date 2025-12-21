0ea5d709527e09111c62bda4a9bf44d103b07537
Klaraaorban <klara.orban@stud.ubbcluj.ro>
2025-12-21 18:48:59 +0200

    browser router change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { motion } from "framer-motion";
 import { Waves } from "lucide-react";
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
